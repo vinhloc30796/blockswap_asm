6d69f0bfcc9d3b5dfca87d2fd47ee9024ecbd4f1
Loc Nguyen <vinhloc30796@gmail.com>
2023-04-10 19:22:39 +0800

    Ping Quicknode with requests

---
+++ client/quicknode.py
@@ -0,0 +1,40 @@
+import os
+import requests
+from typing import Dict
+
+SAMPLE_BLS_KEY = "0x8021cca323eb594a275752032852777098ef5bc7970db412c9c2c10be3f083d1e02fbe5d0992f24e9aa52a9f7815fe59"
+
+
+def get_validators(bls_key: str = SAMPLE_BLS_KEY) -> Dict:
+    """
+    Get validator information from Quicknode API
+
+    Arguments:
+        bls_key {str} -- BLS public key
+
+    Returns:
+        Dict -- Validator information
+
+    Reference: https://www.quicknode.com/docs/ethereum/eth-v1-beacon-states-%7Bstate_id%7D-validators-%7Bvalidator_id%7D
+    """
+    url = os.environ["QUICKNODE_URL"]
+    path = os.getenv("QUICKNODE_PATH", "/eth/v1/beacon/states/finalized/validators/")
+    full_url = f"{url}{path}{bls_key}"
+    print(full_url)
+
+    # Params
+    payload = {}
+    headers = {"accept": "application/json"}
+
+    # Make request with Token Based Auth
+    res = requests.get(full_url, headers=headers, data=payload)
+    return res.json()
+
+
+def main():
+    res = get_validators()
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
